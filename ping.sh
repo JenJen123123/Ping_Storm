b323e06539046274f85183d06350f1ac46c49af4
YoelTokatly <yoel2707@gmail.com>
2025-03-25 11:07:20 +0000

    Ping formated

--- ping.sh
+++ ping.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
-
-# Ping script to check the availability of target services
-
-# List of target services
 services=(
     "google.com"
     "facebook.com"
     "tiktok.com"
     "youtube.com"
-    #"netflix.com"
+    "netflix.com"
 )
 
-# Loop through each service and ping
-for service in "${services[@]}"; do
-    #echo "Pinging ${service}..."
-    echo -e "\e[1;31m ~~~ Pinging \e[36m${service}\e[0m ~~~ \e[0m"
-    ping -c 1 "$service"
-    echo "-----------------------------------"
+num_pings=4
+
+# Print header
+echo "Website       Min      Avg      Max      Mdev"
+echo "----------------------------------------------"
+
+# Loop services
+for site in "${services[@]}"; do
+    ping_result=$(ping -c $num_pings $site 2>/dev/null | grep "^rtt min")
+    #We need to add a packet loss condition
+    if [ ! -z "$ping_result" ]; then
+        # format output
+        good_result=$(echo "$ping_result" | awk -v site="$site" -F'= ' '{
+            # Split the values (min/avg/max/mdev)
+            split($2, a, "/"); 
+            
+            # Remove " ms" from the last value
+            gsub(/ ms/, "", a[4]); 
+            
+            # Format output with fixed width columns
+            printf "%-13s %-8s %-8s %-8s %-8s\n", site, a[1], a[2], a[3], a[4]
+        }')
+        
+        echo "$good_result" 
+    else
+        # If error
+        echo "%-13s %-8s %-8s %-8s %-8s\n" "$site" "ERROR" "ERROR" "ERROR" "ERROR" >>Error.log
+    fi
+    
 done
-
-# End of script
-
-echo " "
-#echo -e "\e[1;31m ~~~ End of script ~~~ \e[0m"
-echo -e "\e[1;33;40m ~~~ End of script ~~~ \e[0m"
-echo " "
+exit 0
