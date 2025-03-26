e022f456fd86fd36481f21c4db85706481ff8ea8
EC2 Default User <ec2-user@ip-172-31-41-51.us-west-2.compute.internal>
2025-03-26 09:09:26 +0000

    control fixes

--- control.sh
+++ control.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 options=("Start" "Stop" "Status" "Show" "Quit")
+PING_PID=0
 
 select comd in "${options[@]}"; do
 	case $comd in
@@ -8,11 +9,21 @@ select comd in "${options[@]}"; do
 			echo "    "
 			echo -e "\e[1;33;40m ~~~ Starting Ping Storm ~~~ \e[0m"
 			echo "    "
-			`./ping.sh`
+			if [[ $PING_PID -ne 0 ]] && ps -p $PING_PID > /dev/null; then
+				echo "PingStorm is already running (PID: $PING_STORM)"
+			else
+				./ping.sh > pingstorm.log 2>&1 &
+				PING_PID=$!
+				echo "PingStorm started in background (PID : $PING_PID)"
+				echo "Output is beingsaved to pingstorm.log"
+			fi
 			;;
 		"Stop")
+			##continue stop option##
 			echo "    "
 			echo "Stoping..."
+			if [[ $PING_PID -ne "0" ]]; then
+
 			;;
 		"Status")
 			echo "    "
