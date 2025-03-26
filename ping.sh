de56d97d062bdd163e0651124c98aa8af9d5f183
JenJen123123 <ssystem32@gmail.com>
2025-03-26 13:41:47 +0200

    Copied noya branch

--- ping.sh
+++ ping.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
+# Ping script to check the availability of target services
 
-# Script to ping a list of websites and display the results in a formatted table
-
-# Function to handle errors
-error() {
-    echo "$(date '+%Y %m %d %H:%M:%S')" "$site" "Didn't send any answer" >>Error.log
-}
-
-# List of services to ping
-# You can add or remove services from this list
-# The script will ping each service and display the results
+# List of target services
 services=(
     "google.com"
     "facebook.com"
@@ -18,44 +10,27 @@ services=(
     "netflix.com"
 )
 
-# Number of pings to send
-num_pings=1 # Change this to the number of pings you want to send
-
-# Print header
-echo " "
-echo -e "\e[1;31;41mWebsite       Min      Avg      Max      Mdev\e[0m"
-echo "----------------------------------------------"
-#echo "Website       Min      Avg      Max      Mdev" >>DataLog.txt
-echo "----------------------------------------------" >>DataLog.txt
-
-# Loop services
-for site in "${services[@]}"; do
-    ping_result=$(ping -c $num_pings $site 2>/dev/null | grep "^rtt min")
-    #We need to add a packet loss condition
-    if [ ! -z "$ping_result" ]; then
-        # format output
-        good_result=$(echo "$ping_result" | awk -v site="$site" -F'= ' '{
-            # Split the values (min/avg/max/mdev)
-            split($2, a, "/"); 
-            
-            # Remove " ms" from the last value
-            gsub(/ ms/, "", a[4]); 
-            
-            # Format output with fixed width columns
-            printf "%-13s %-8s %-8s %-8s %-8s\n", site, a[1], a[2], a[3], a[4]
-        }')
-
-        echo "$good_result"
-        echo "$good_result" >>DataLog.txt
-    else
-        # If error, print error message with date and site name to Error.log
-        error
-    fi
-
+# Loop through each service and ping
+for service in "${services[@]}"; do
+    #echo "Pinging ${service}..."
+    echo -e "\e[1;31m ~~~ Pinging \e[36m${service}\e[0m ~~~ \e[0m"
+    ping -c 1 "$service"
+    echo "-----------------------------------"
 done
 
-echo "----------------------------------------------"
-echo -e "\e[1;31;44m~~~~~~~~~~~~~ End Of Ping Script ~~~~~~~~~~~~~\e[0m"
+# End of script
+
+echo " "
+#echo -e "\e[1;31m ~~~ End of script ~~~ \e[0m"
+echo -e "\e[1;33;40m ~~~ End of script ~~~ \e[0m"
 echo " "
 
-#exit 0
+
+ping -c 5 8.8.8.8
+if [[ $? -eq 0 ]];then
+echo "ok"
+else echo "prob"
+fi
+if [[ $service 
+
+
---
+++ pingtest.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+#!/bin/bash
+
+# Script to ping a list of websites and display the results in a formatted table
+
+# Function to handle errors
+error() {
+    echo "$(date '+%Y %m %d %H:%M:%S')" "$site" "Didn't send any answer" >>Error.log
+}
+
+# List of services to ping
+# You can add or remove services from this list
+# The script will ping each service and display the results
+services=(
+    "google.com"
+    "facebook.com"
+    "tiktok.com"
+    "youtube.com"
+    "netflix.com"
+)
+
+# Number of pings to send
+num_pings=1 # Change this to the number of pings you want to send
+
+# Print header
+echo " "
+echo -e "\e[1;31;41mWebsite       Min      Avg      Max      Mdev\e[0m"
+echo "----------------------------------------------"
+#echo "Website       Min      Avg      Max      Mdev" >>DataLog.txt
+echo "----------------------------------------------" >>DataLog.txt
+
+# Loop services
+for site in "${services[@]}"; do
+    ping_result=$(ping -c $num_pings $site 2>/dev/null | grep "^rtt min")
+    #We need to add a packet loss condition
+    if [ ! -z "$ping_result" ]; then
+        # format output
+        good_result=$(echo "$ping_result" | awk -v site="$site" -F'= ' '{
+            # Split the values (min/avg/max/mdev)
+            split($2, a, "/"); 
+            
+            # Remove " ms" from the last value
+            gsub(/ ms/, "", a[4]); 
+            
+            # Format output with fixed width columns
+            printf "%-13s %-8s %-8s %-8s %-8s\n", site, a[1], a[2], a[3], a[4]
+        }')
+
+        echo "$good_result"
+        echo "$good_result" >>DataLog.txt
+    else
+        # If error, print error message with date and site name to Error.log
+        error
+    fi
+
+done
+
+echo "----------------------------------------------"
+echo -e "\e[1;31;44m~~~~~~~~~~~~~ End Of Ping Script ~~~~~~~~~~~~~\e[0m"
+echo " "
+
+#exit 0
